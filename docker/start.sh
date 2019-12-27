15b1a49d067d2e2e1cfd2330ab9580f05da23a72
René Zubcevic <rene@zubcevic.com>
2019-12-27 20:35:15 +0100

    prepare for docker

--- docker/start.sh
+++ docker/start.sh
@@ -4,10 +4,10 @@
 ##pkill -9 -f webwolf.jar
 cd /home/webgoat 
 sleep 1
-java -Dfile.encoding=UTF-8 -jar webgoat.jar --server.address=0.0.0.0  > webgoat.log &
+java -Dfile.encoding=UTF-8 -jar webgoat.jar --webgoat.build.version=8.0-8088465 --server.address=0.0.0.0  > webgoat.log &
 
 sleep 10
  
-java -Dfile.encoding=UTF-8 -jar webwolf.jar --server.address=0.0.0.0 > webwolf.log &
+java -Dfile.encoding=UTF-8 -jar webwolf.jar --webgoat.build.version=8.0-8088465 --server.address=0.0.0.0 > webwolf.log &
 
 tail -300f webgoat.log
