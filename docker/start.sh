7bfc114a5d8638fe8d90aef72498f9272c1d3221
René Zubcevic <rene@zubcevic.com>
2020-01-18 08:23:36 +0100

    build args env added

--- docker/start.sh
+++ docker/start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-##pkill -9 -f webgoat-server.jar
-##pkill -9 -f webwolf.jar
 cd /home/webgoat 
 sleep 1
-java -Dfile.encoding=UTF-8 -jar webgoat.jar --webgoat.build.version=8.0-8088465 --server.address=0.0.0.0  > webgoat.log &
+java -Dfile.encoding=UTF-8 -jar webgoat.jar --webgoat.build.version=$1 --server.address=0.0.0.0  > webgoat.log 
 
 sleep 10
  
-java -Dfile.encoding=UTF-8 -jar webwolf.jar --webgoat.build.version=8.0-8088465 --server.address=0.0.0.0 > webwolf.log &
+java -Dfile.encoding=UTF-8 -jar webwolf.jar --webgoat.build.version=$1 --server.address=0.0.0.0 > webwolf.log &
 
 tail -300f webgoat.log
