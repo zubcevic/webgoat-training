58b3bbcef0138045683c9776a65b243272526cdd
René Zubcevic <git@zubcevic.com>
2021-11-08 10:43:38 +0100

    Create test.py

---
+++ nondefault/test.py
@@ -0,0 +1,12 @@
+print ('begin chartest.py')
+
+def do_something_weird():
+    print('begin do_something_weird')
+
+    '''do something then ⁧''' ;return
+
+    print('end do_something_weird')
+
+do_something_weird()
+
+print ('end chartest.py')
